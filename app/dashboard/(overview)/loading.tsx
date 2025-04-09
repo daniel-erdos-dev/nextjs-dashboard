64770be4fd02ab9aaace1139d1ec19d3480a2772
Dániel Erdős <13763460+daniel-erdos-dev@users.noreply.github.com>
2025-04-09 16:04:21 +0200

    Chapter 2

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardSkeleton from "../../ui/skeletons";
+
+const Loading: React.FC = () => {
+  return <DashboardSkeleton />;
+};
+
+export default Loading;
--- app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DashboardPage: React.FC = () => {
-  return <p>Dashboard Page</p>;
-};
-
-export default DashboardPage;
