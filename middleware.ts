cb90106c5dce81e99c029ad86d6eb87d3db53f59
Dániel Erdős <13763460+daniel-erdos-dev@users.noreply.github.com>
2025-04-10 18:05:48 +0200

    Chapter 15

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import { NextRequest, NextResponse } from "next/server";
+
+export default NextAuth(authConfig).auth;
+
+export function middleware(request: NextRequest) {}
+
+export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
